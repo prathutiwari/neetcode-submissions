02e1986f52cb4b3a956f677ede73c199280d230f
Prathu Tiwari <prathutiwari22@gmail.com>
2026-07-11 18:42:29 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let newObj = {};
+
+        for(let i = 0; i < nums.length; i++){
+            if(newObj[nums[i]]){
+                newObj[nums[i]]++;
+                return true;
+            }else{
+                newObj[nums[i]] = 1;
+            }
+        }
+        return false
+    }
+}
