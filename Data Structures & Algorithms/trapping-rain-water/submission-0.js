a829631a186655f46eba0e8d944c1878d0b0507b
Prathu Tiwari <prathutiwari22@gmail.com>
2026-09-17 08:43:26 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(arr) {
+        let n = arr.length;
+
+        let maxL = [];
+        maxL[0] = arr[0];
+        for(let i = 1; i < n; i++){
+            maxL[i] = Math.max(maxL[i-1], arr[i]);
+        }
+
+        let maxR = [];
+        maxR[n-1] = arr[n-1];
+        for(let i = n-2; i >= 0; i--){
+            maxR[i] = Math.max(arr[i], maxR[i+1]);
+        }
+
+        let ans = 0;
+        for(let i = 0; i < n; i++){
+            let waterTrapped = Math.min(maxL[i], maxR[i]) - arr[i];
+            ans = ans + Math.max(waterTrapped, 0)
+        }
+
+        return ans
+
+    }
+}
